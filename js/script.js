5a19d483b19b3340fd8bbd6ddc8aeea857bf5a79
allyson_s <allysonsmithstudio@gmail.com>
2022-04-03 09:43:43 -0400

    Tidying up code

--- js/script.js
+++ js/script.js
@@ -4,27 +4,17 @@ const thankYouState = document.querySelector(".thankyou-state-container");
 const submitButton = document.querySelector(".submit-button");
 const userRating = document.querySelector(".survey-result-text .rating");
 
-//click event and obtain value of rating
+//click event and obtain value of rating and update survey result text in thank you state
 for (const button of ratingButtons) {
   button.addEventListener("click", function () {
     const value = button.value;
-    //console.log(value);
     button.classList.add("button.active");
     userRating.innerText = `${value}`;
   });
 }
 
-//function to update the survey result text in thank you state
-
-//submit user rating value and update survey result span and show thank you state
+//submit user rating value and show thank you state
 submitButton.addEventListener("click", function () {
   ratingState.classList.add("hide");
   thankYouState.classList.remove("hide");
 });
-
-//ratingButtons.forEach((button) => {
-//button.addEventListener("click", () => {
-//const value = button.value;
-//console.log(value);
-//});
-//});
