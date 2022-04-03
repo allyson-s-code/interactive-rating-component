ed376621d37a26653ce58115913d85210b72cc8f
allyson_s <allysonsmithstudio@gmail.com>
2022-04-03 08:57:11 -0400

    Completed js interactivity tweaked css and html

--- js/script.js
+++ js/script.js
@@ -2,15 +2,29 @@ const ratingState = document.querySelector(".rating-state-container");
 const ratingButtons = document.querySelectorAll(".rating-button");
 const thankYouState = document.querySelector(".thankyou-state-container");
 const submitButton = document.querySelector(".submit-button");
+const userRating = document.querySelector(".survey-result-text .rating");
 
-ratingButtons.forEach((button) => {
-  button.addEventListener("click", () => {
+//click event and obtain value of rating
+for (const button of ratingButtons) {
+  button.addEventListener("click", function () {
     const value = button.value;
-    console.log(value);
+    //console.log(value);
+    button.classList.add("button.active");
+    userRating.innerText = `${value}`;
   });
+}
+
+//function to update the survey result text in thank you state
+
+//submit user rating value and update survey result span and show thank you state
+submitButton.addEventListener("click", function () {
+  ratingState.classList.add("hide");
+  thankYouState.classList.remove("hide");
 });
 
-submitButton.addEventListener("click", function () {
-  ratingState.classList.add(".hide");
-  thankYouState.classList.remove(".hide");
-});
+//ratingButtons.forEach((button) => {
+//button.addEventListener("click", () => {
+//const value = button.value;
+//console.log(value);
+//});
+//});
